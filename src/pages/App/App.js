67366e6e0dedd7b18296f48ff2185291ff12b36b
Richard Mundy <mundy.richard@icloud.com>
2024-05-22 19:20:35 -0700

    general layout

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes></Routes>
     </div>
   );
 }
